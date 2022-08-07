11ebdc66214470ce7a42dfdef90ed1f61e7e1e8d
Nordfox88 <105307042+Nordfox88@users.noreply.github.com>
2022-08-07 16:34:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-      int ticket = 1000;   // Объявляете переменные для входных данных и
-      int OneBonusMille = 20;  // параметров программы: одну для хранения
+      int ticket = 1000;   
+      int OneBonusMille = 20;  
 
-        System.out.println("Получите бонусных миль:");  // стоимости билета, другую для хранения количества
-        System.out.println((ticket / OneBonusMille) + " миль. ");// рублей для одной бонусной милли
+        System.out.println("Получите бонусных миль:");  
+        System.out.println((ticket / OneBonusMille) + " миль. ");
 
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        
     }
 }
