349ab4c0741d107687398ba0542f44efd486f715
Nordfox88 <105307042+Nordfox88@users.noreply.github.com>
2022-08-11 20:22:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
       int ticket = 1000;   
-      int OneBonusMille = 20;  
+      int oneBonusMille = 20;  
 
         System.out.println("Получите бонусных миль:");  
         System.out.println((ticket / OneBonusMille) + " миль. ");
